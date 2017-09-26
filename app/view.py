5fb3d78a3c44105d10bf86b2d3fdcee83e1e682e
iegor <timukhin@ukr.net>
2017-09-26 13:00:04 +0300

    Add Flask-Cors for running to app in one PC

--- app/view.py
+++ app/view.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, jsonify # iimport from flask library
+from flask_cors import cross_origin
 
 from app import app # import from __init__.py
 
@@ -12,13 +13,13 @@ def root():
     return render_template('index.html')
 
 @app.route('/run')
+@cross_origin()
 def run_script():
     """Accepting AJAX request and rendering JSON response
     in format:
     data = {
         'name': '', <= Name that accepted from request
         'data': None, <= Data from the script
-        'status': '', <= Status if the script is finding 200 else 400
         'message': '' <= OK if everything is good, else Not found this function
     }
 
@@ -26,18 +27,15 @@ def run_script():
     data = {
         'name': '',
         'data': None,
-        'status': '',
         'message': ''
     }
     script_name = request.args['script_name']
     data['name'] = script_name
     data_script = controller.run_script(script_name)
     if data_script:
-        data['status'] = 200
         data['message'] = 'OK'
         data['data'] = data_script
     else:
-        data['status'] = 400
         data['message'] = 'Not found this function'
     return jsonify(data)
 
